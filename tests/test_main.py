d37d8befc580a1ca9663bf943587cd88f16fcb5c
sashaadonev-web <sashaadonev@mail.ru>
2026-06-02 08:29:16 +0300

    исправил test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,6 @@ client = TestClient(app)
 
 def test_calculate_bmi_success():
     # Проверяем расчет для человека весом 70 кг и ростом 1.75 м
-    response = client.get("/bmi?weight=70&height=1.75")
+    response = client.get("/laba1?weight=70&height=1.75")
     assert response.status_code == 200
-    assert response.json()["bmi"] == 22.86
+    assert response.json()["laba1"] == 22.86
